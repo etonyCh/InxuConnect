4f769bb7ac0ac162608113c4c3766b327601dd10
Tony Charmant <egerimanatonycharmant@gmail.com>
2026-08-02 08:43:40 +0100

    refactor: purge dead React/Next.js code and enforce modular microservices architecture

--- apps/web/next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  allowedDevOrigins: ['192.168.1.20', 'localhost'],
-}
-
-module.exports = nextConfig
--- apps/web/src/components/MapViewerWrapper.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import dynamic from 'next/dynamic'
-
-const MapViewer = dynamic(() => import('./MapViewer'), { ssr: false })
-
-export default function MapViewerWrapper(props: { position: { lat: number, lng: number } }) {
-  return <MapViewer {...props} />
-}
